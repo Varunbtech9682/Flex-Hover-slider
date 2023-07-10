1efab60653c26c033be2872181dbd00fe3d16786
varun <varunbtech9682@gmail.com>
2023-07-10 17:36:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+//Code By Webdevtrick ( https://webdevtrick.com )
+(function() {
+    $('.flex-container').waitForImages(function() {
+        $('.spinner').fadeOut();
+    }, $.noop, true);
+
+    $(".flex-slide").each(function() {
+        $(this).hover(function() {
+            $(this).find('.flex-title').css({
+                transform: 'rotate(0deg)',
+                top: '10%'
+            });
+            $(this).find('.flex-about').css({
+                opacity: '1'
+            });
+        }, function() {
+            $(this).find('.flex-title').css({
+                transform: 'rotate(90deg)',
+                top: '15%'
+            });
+            $(this).find('.flex-about').css({
+                opacity: '0'
+            });
+        })
+    });
+})();
